5f3699d613fa71812ea5c7b0c1fe46a8f7b8a7f3
unknown <candyjimenez90@yahoo.com>
2013-06-07 18:24:31 -0700

    first commit

---
+++ HighScores.java
@@ -0,0 +1,40 @@
+/*************************************************************** 
+* file: Colors.java 
+* authors: Yvonne Hong, Candy Jimenez
+* class: CS 245 – Programming Graphical User Interface 
+* 
+* assignment: Quarter Project program 1 
+* date last modified: 3/18/2013
+*  
+* purpose: This class changes the way the Highscores frame would look
+* like
+* 
+****************************************************************/
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+public class HighScores extends JComponent{
+	// method: paintComponent
+	// purpose: changes the way the highScore menu looks like
+      public void paintComponent(Graphics g) {
+      if(g instanceof Graphics2D)
+      {
+        Graphics2D g2 = (Graphics2D)g;
+        Font font = new Font("Arial", Font.BOLD, 72);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(Color.WHITE);
+        g2.setFont(font);
+        g2.drawString("High Scores",90,60); 
+      }
+    }
+}
